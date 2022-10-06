9c30515e6162e8a3f7b4c2166f76efc6ba5195ea
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-10-06 13:03:50 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d ",&n);     for(int i=1 ; i<=n ; i++){             if(n%2!=0){                 for(int j=1 ; j<=n-2 ; j++){                     printf("%d",j);                 }                 for(int k=1 ; k<n-2 ; k++){                     printf("%d",k);                 }                 printf(" ");             }             else{                 for(int j=1 ; j<=n-2 ; j++){                     printf("%d",j);                 }                 for(int k=1 ; k<n-2 ; k++){                     printf("%d",k);                 }                 printf(" ");             }         } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,11 +3,26 @@ int main()
 {
     int n;
     scanf("%d ",&n);
-    for(int i=0 ; i<n ; i++){
-            for(int j=n ; j>=1 ; j--){
-                printf("%d ",j);
-            }
-            printf("
+    for(int i=1 ; i<=n ; i++){
+            if(n%2!=0){
+                for(int j=1 ; j<=n-2 ; j++){
+                    printf("%d",j);
+                }
+                for(int k=1 ; k<n-2 ; k++){
+                    printf("%d",k);
+                }
+                printf("
 ");
+            }
+            else{
+                for(int j=1 ; j<=n-2 ; j++){
+                    printf("%d",j);
+                }
+                for(int k=1 ; k<n-2 ; k++){
+                    printf("%d",k);
+                }
+                printf("
+");
+            }
         }
 }
